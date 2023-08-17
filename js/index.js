958f0209071f095349d26b27512b413276c0060e
João Paulo Ersching <joao.ersching@gmail.com>
2023-08-17 12:24:51 -0300

    Add menu

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function menu() {
+    let menu = document.getElementById('menuContainer');
+
+    if(menu.style.display === 'none'){
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    }
+}
