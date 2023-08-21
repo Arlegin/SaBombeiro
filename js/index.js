fe276f237a1ffd843cb23d411917c4ff0636cc77
João Paulo Ersching <joao.ersching@gmail.com>
2023-08-21 08:15:18 -0300

    Add menu animation

--- js/index.js
+++ js/index.js
@@ -1,9 +1,22 @@
 function menu() {
     let menu = document.getElementById('menuContainer');
+    let icon = document.getElementById('menuCloseIcon')
 
-    if(menu.style.display === 'none'){
+    if (menu.style.display === 'none') {
+        setTimeout(() => {
+            icon.style.display = 'block'
+        }, 150);
         menu.style.display = 'block';
+        menu.classList.remove('closeMenu');
+        menu.classList.add('openMenu');
     } else {
-        menu.style.display = 'none';
+        setTimeout(() => {
+            icon.style.display = 'none'
+        }, 250);
+        menu.classList.remove('openMenu');
+        menu.classList.add('closeMenu');
+        setTimeout(() => {
+            menu.style.display = 'none';
+        }, 600);
     }
 }
